7ad7ac8cda5bf0673113b2ae0aaab3d62bb3ca98
Daquiz24 <daquizcunananmark@gmail.com>
2024-01-28 23:57:20 +0800

    daquiz

--- homepage.php
+++ homepage.php
@@ -50,7 +50,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Your Logo</a>
+    <a class="navbar-brand" href="#">GEISHA'S BLADE 芸者の刃</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
